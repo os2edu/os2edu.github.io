2e9db2d415b6da8502d82e84a217f5399f7b5453
DongJianHao123 <2995251733@qq.com>
2023-09-19 18:18:10 +0800

    fix: add form check and feedback

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { sendEmail } from './utils'
 import ReactDOM from 'react-dom/client'
 
 const EmailForm = () => {
+  const [isSuccess, setIsSuccess] = useState<boolean>(false)
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.stopPropagation()
     e.preventDefault()
@@ -15,13 +16,20 @@ const EmailForm = () => {
     const email = target.email.value
     const message = target.message.value;
     sendEmail(name, email, message)
+    setIsSuccess(true)
+    setTimeout(() => {
+      setIsSuccess(false)
+    }, 3000)
+    target.name.value = '';
+    target.email.value = '';
+    target.message.value = '';
   }
   return (
     <form id="contact" onSubmit={handleSubmit}>
       <div className="row">
         <div className="col-lg-6 col-md-12 col-sm-12">
           <fieldset>
-            <input name="name" type="text" className="form-control" id="name" placeholder="姓名" />
+            <input name="name" type="text" className="form-control" id="name" placeholder="姓名" required />
           </fieldset>
         </div>
         <div className="col-lg-6 col-md-12 col-sm-12">
@@ -32,12 +40,14 @@ const EmailForm = () => {
               className="form-control"
               id="email"
               placeholder="邮箱"
+              required
             />
           </fieldset>
         </div>
         <div className="col-lg-12">
           <fieldset>
             <textarea
+              required
               name="message"
               rows={6}
               className="form-control"
@@ -47,10 +57,18 @@ const EmailForm = () => {
           </fieldset>
         </div>
         <div className="col-lg-12">
-          <fieldset>
+          <fieldset style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '10px'
+          }}>
             <button id="form-submit" className="main-button" style={{ width: 120 }}>
-              发信息
+              发送
             </button>
+            {isSuccess && <span style={{ color: '#52c41a', transition: 'all 2s' }}>√ 发送成功</span>}
           </fieldset>
         </div>
       </div>
