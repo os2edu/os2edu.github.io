4ae2bc284de31ac4f049c4a49936dcac1ffd5f26
DongJianHao123 <2995251733@qq.com>
2023-09-26 18:11:16 +0800

    fix: change footer and link ranking -> standings

---
+++ src/footer.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+
+const Footer = () => {
+    const [currLabel, setCurrLabel] = useState<string>('')
+
+    const links: { label: string, icon: string, url: string, type: 'img' | 'char' }[] = [
+        {
+            label: '清华大学开源操作系统研究组',
+            icon: '/public/assets/images/qinghua.png',
+            url: 'https://os.cs.tsinghua.edu.cn',
+            type: 'img'
+        }, {
+            label: 'rCore OS',
+            icon: 'R',
+            url: 'https://rcore-os.cn',
+            type: 'char'
+        }, {
+            label: '操作系统学习博客',
+            icon: 'B',
+            url: 'https://rcore-os.cn/blog/',
+            type: 'char'
+        }, {
+            label: '开源操作系统年度技术会议',
+            icon: '/public/assets/images/os2atc.png',
+            url: 'https://os2atc.cn',
+            type: 'img'
+        }, {
+            label: '开源操作系统训练营',
+            icon: '/public/assets/images/github.png',
+            url: 'https://github.com/LearningOS',
+            type: 'img'
+        }
+    ]
+
+    return <footer>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12 col-md-12 col-sm-12">
+                    <ul className='footer-ul'>
+                        {links.map((item) => {
+                            return <li className='footer-li' onMouseLeave={()=>setCurrLabel('')} onMouseEnter={() => setCurrLabel(item.label)}><a href={item.url} target='_blank'>
+                                {
+                                    item.type === 'img' ? <img style={{border:'2px solid #fff', width: '100%', height: '100%', borderRadius: '50%' }} src={item.icon} alt="" />
+                                        : <span className='footer-char'>{item.icon}</span>
+                                }
+                            </a></li>
+                        })}
+                    </ul>
+                 <span className='curr-label'>{currLabel}</span>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-12">
+                    <p className="copyright">&copy; 北京清华大学·京ICP备16045052号-11</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+}
+
+ReactDOM.createRoot(document.getElementById('footer') as HTMLElement).render(<Footer />)
+
+export default Footer
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { sendEmail } from './utils'
 import ReactDOM from 'react-dom/client'
+import './footer.css'
 
 const EmailForm = () => {
   const [isSuccess, setIsSuccess] = useState<boolean>(false)
@@ -25,54 +26,58 @@ const EmailForm = () => {
     target.message.value = '';
   }
   return (
-    <form id="contact" onSubmit={handleSubmit}>
-      <div className="row">
-        <div className="col-lg-6 col-md-12 col-sm-12">
-          <fieldset>
-            <input name="name" type="text" className="form-control" id="name" placeholder="姓名" required />
-          </fieldset>
+    <div>
+
+      <form id="contact" onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-lg-6 col-md-12 col-sm-12">
+            <fieldset>
+              <input name="name" type="text" className="form-control" id="name" placeholder="姓名" required />
+            </fieldset>
+          </div>
+          <div className="col-lg-6 col-md-12 col-sm-12">
+            <fieldset>
+              <input
+                name="email"
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="邮箱"
+                required
+              />
+            </fieldset>
+          </div>
+          <div className="col-lg-12">
+            <fieldset>
+              <textarea
+                required
+                name="message"
+                rows={6}
+                className="form-control"
+                id="message"
+                placeholder="内容"
+              ></textarea>
+            </fieldset>
+          </div>
+          <div className="col-lg-12">
+            <fieldset style={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              alignItems: 'center',
+              gap: '10px'
+            }}>
+              <button id="form-submit" className="main-button" style={{ width: 120 }}>
+                发送
+              </button>
+              {isSuccess && <span style={{ color: '#52c41a', transition: 'all 2s' }}>√ 发送成功</span>}
+            </fieldset>
+          </div>
         </div>
-        <div className="col-lg-6 col-md-12 col-sm-12">
-          <fieldset>
-            <input
-              name="email"
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="邮箱"
-              required
-            />
-          </fieldset>
-        </div>
-        <div className="col-lg-12">
-          <fieldset>
-            <textarea
-              required
-              name="message"
-              rows={6}
-              className="form-control"
-              id="message"
-              placeholder="内容"
-            ></textarea>
-          </fieldset>
-        </div>
-        <div className="col-lg-12">
-          <fieldset style={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            alignItems: 'center',
-            gap: '10px'
-          }}>
-            <button id="form-submit" className="main-button" style={{ width: 120 }}>
-              发送
-            </button>
-            {isSuccess && <span style={{ color: '#52c41a', transition: 'all 2s' }}>√ 发送成功</span>}
-          </fieldset>
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
+
   )
 }
 
